b184ecb5e738564ee68480f9cf61519f043685cf
samukele <samukele107@gmail.com>
2023-04-21 06:22:20 +0200

    aaa

--- my_resume/settings.py
+++ my_resume/settings.py
@@ -134,12 +134,6 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 APPEND_SLASH = False
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'samukele107@gmail.com'
-EMAIL_HOST_PASSWORD = 'Samukele1997'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
